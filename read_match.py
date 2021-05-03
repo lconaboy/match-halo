82d718b71235cf4ebc53d56d743d2a3943f65344
Luke <lukeconaboy@gmail.com>
2021-05-03 18:24:16 +0100

    Add more I/O options

--- read_match.py
+++ read_match.py
@@ -1,4 +1,24 @@
+"""
+The format of the output file is each row is the ID of matched halo,
+with each column being the different simulations.  The final column is
+always the minimum maatched fraction for that halo.  The header of the
+output file should have the form
+
+# <match1> <match2> ... <matchN>
+
+/without/ <match_frac> on the end.  <matchi> should be the identifier
+of the run, i.e. same_vtf.
+"""
+
 import numpy as np
+
+def get_dtype_match3(fields):
+    dtype = [(f, 'i8') for f in fields]
+    dtype.append(('match_frac', 'f4'))
+    dtype_match3 = np.dtype(dtype)
+
+    return dtype_match3
+
 
 def write_match(iout, match):
     """Writes matched array to an ASCII file.
@@ -15,6 +35,36 @@ def write_match(iout, match):
     np.savetxt('match_{0:d}.list'.format(iout), match, fmt='%d %d %.4f')
 
 
+def write_match_three(iout, match, match_frac, fields=None):
+    """Writes matched array to an ASCII file.
+
+    :param iout: (int) output number
+    :param match: (arr) matched outputs, first column is halo ID,
+        second column is halo ID, third column is halo ID and final
+        column is match fraction
+
+    :returns: None
+
+    :rtype: (NoneType)
+    """
+
+    if fields is None:
+        fields = ['match1', 'match2', 'match3']
+
+    header = ' '.join(fields)
+    dtype_match3 = get_dtype_match3(fields)
+    
+    out = np.zeros((match_frac.shape[0]), dtype=dtype_match3)
+    for i, f in enumerate(fields):
+        out[f] = match[:, i]
+        out[f] = match[:, i]
+        out[f] = match[:, i]
+    out['match_frac'] = match_frac
+        
+    np.savetxt('match_{0:d}.list'.format(iout), out, fmt='%d %d %d %.4f',
+               header=header)
+
+
 def read_match(iout):
     """Reads matched array from ASCII file.
 
@@ -26,8 +76,42 @@ def read_match(iout):
     :rtype: (arr)
     """
     
-    # match = np.loadtxt('match_{0:d}.list'.format(iout), fmt='%d %d %.4f')
+
     match = np.genfromtxt('match_{0:d}.list'.format(iout))
-#                          dtype=[('id1', int), ('id2', int),
-#                                 ('match_frac', float)])
+
     return match
+
+
+def read_match3(iout):
+    """Reads matched array from ASCII file.
+
+    :param iout: (int) output number
+
+    :returns: matched outputs, first column is halo ID, second
+        column is halo ID and final column is match fraction
+
+    :rtype: (struct arr)
+    """
+
+    # First regenerate the field names from the header
+    match_fn = get_match_fn(iout)
+    fields = read_header(iout)
+    dtype_match3 = get_dtype_match3(fields)
+    # Now load the data
+    match = np.genfromtxt(match_fn, dtype=dtype_match3)
+
+    return match
+
+
+def read_header(iout):
+    match_fn = get_match_fn(iout)
+    with open(match_fn, 'r') as f:
+        header = f.readline()
+        
+    fields = header[1:].strip('\n').split()
+
+    return fields
+
+
+def get_match_fn(iout):
+    return 'match_{0:d}.list'.format(iout)
